311c155d60481eba20156b6ce508a71ac0f477f1
jambrocio-2022262 <jambrocio-2022262@kinal.edu.gt>
2024-02-27 13:56:09 -0600

    Finish to the user.model this proyect

--- src/users/user.model.js
+++ src/users/user.model.js
@@ -1,8 +1,34 @@
 import mongoose from "mongoose";
 
 const UserSchema = mongoose.Schema({
-    nombre: {
+    name:{
         type: String,
-        require: [true, "The name is required"]
+        require: [true, "The Name is required"]
+    },
+    UserName: {
+        type: String,
+        require: [true, "The User is required"]
+    },
+    email:{
+        type: String,
+        require: [true, "The Email is required"],
+        unique: true
+    },
+    password:{
+        type: String,
+        require: [true, "The Paswword is required"]
+    },
+    estado:{
+        type: Boolean,
+        default: true
     }
-})
+
+});
+
+UserSchema.methods.toJSON = function(){
+    const{ __v, password, _id, ...usuario} = this.toObject();
+    usuario.uid = _id;
+    return usuario;
+};
+
+export default mongoose.model('User',UserSchema);
